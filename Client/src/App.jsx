d7b4b9ed5694fb080d83f12ac17861fa1e07d56e
RABBI HASAN <130126798+CodeRabbi4@users.noreply.github.com>
2025-09-13 10:36:45 +0600

    Add client folder

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <div className='text-red-500 text-4xl'>Aura AI
+      <h1>Hello worldffffffffffffffff
+      
+      </h1>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App;
